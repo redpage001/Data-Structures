a6c728d1ffddfed5efc12b56e67386a98b0dc6d0
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-06-08 14:53:01 -0700

    Completed day 1 mvp for project by completing tasks for the queue and stack files with implementation of simply_linked_list

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,126 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next_node = next
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, data):
+        new_node = Node(data)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+    
+    def add_to_head(self, value):
+        newNode = Node(value)
+
+        if not self.head and not self.tail:
+            self.head = newNode
+            self.tail = newNode
+
+        else:
+            newNode.set_next(self.head)
+            self.head = newNode
+
+    def remove_tail(self):
+        if self.tail is None:
+            return None
+        
+        data = self.tail.get_value()
+        
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        
+        else:
+            current = self.head
+            while current.get_next() != self.tail:
+                current = current.get_next()
+            self.tail = current
+        
+        return data
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        
+        data = self.head.get_value()
+        
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+       
+        else:
+            self.head = self.head.get_next()
+
+        return data
+
+    def contains(self, data):
+        if not self.head:
+            return False
+
+        current = self.head 
+
+        while current is not None:
+            if current.get_value() == data:
+                return True
+
+            current = current.get_next()
+        
+        return False
+
+    def get_max(self):
+        if self.head is None:
+            return None
+
+        max_so_far = self.head.get_value()
+
+        current = self.head.get_next()
+
+        while current is not None:
+            if current.get_value() > max_so_far:
+                max_so_far = current.get_value()
+
+            current = current.get_next()
+            
+        return max_so_far
